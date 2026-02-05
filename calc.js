55387892be5ff83a2f46f8136f2dc547ca6bc96f
Mia OHalloran <miaohall@bu.edu>
2026-02-04 23:40:48 -0500

    working calculator

--- calc.js
+++ calc.js
@@ -1,13 +1,14 @@
 let result;
+let output;
 
 function addition() {
     let num1 = document.getElementById("first-number").value;
     let num2 = document.getElementById("second-number").value;
     num1 = Number(num1);
     num2 = Number(num2);
-    result = num1 + num2;
-    document.getElementById("output").innerHTML = String(result);
 
+    result = num1 + num2;
+    printResult(result);
 }
 
 function minus() {
@@ -15,8 +16,9 @@ function minus() {
     let num2 = document.getElementById("second-number").value;
     num1 = Number(num1);
     num2 = Number(num2);
+
     result = num1 - num2;
-    document.getElementById("output").innerHTML = String(result);
+    printResult(result);
 }
 
 function multiply() {
@@ -24,36 +26,56 @@ function multiply() {
     let num2 = document.getElementById("second-number").value;
     num1 = Number(num1);
     num2 = Number(num2);
+
     result = num1 * num2;
-    document.getElementById("output").innerHTML = String(result);
+    printResult(result);
 }
+
 
 function divide() {
     let num1 = document.getElementById("first-number").value;
     let num2 = document.getElementById("second-number").value;
     num1 = Number(num1);
     num2 = Number(num2);
+
     if (num2 === 0) {
-        result = "error - can't divide by 0"
+        result = "ERROR - can't divide by 0. Try again"
     }
     else {
         result = num1/num2;
     }
-    document.getElementById("output").innerHTML = String(result);
+    printResult(result);
 }
 
 function power() {
+    result = 1;
     let num1 = document.getElementById("first-number").value;
     let num2 = document.getElementById("second-number").value;
     num1 = Number(num1);
     num2 = Number(num2);
-    for (let i=0; i<n; i++) {
+
+    for (let i=0; i<num2; i++) {
         result*=num1;
     }
-    document.getElementById("output").innerHTML = String(result);
+    printResult(result);
 }
 
-function clear() {
+function printResult(r) {
+    output = document.getElementById("output");
+    /*negative results in red, positive results reset to default color*/
+    if (r < 0) {
+        output.style.color = "red";
+    }
+    else {
+        output.style.color = "#113f67";
+    }
+    output.innerHTML = String(r);
+}
 
+function reset() {
+    result = "";
+    document.getElementById("first-number").value = "";
+    document.getElementById("second-number").value = "";
+    document.getElementById("output").innerHTML = "";
 }
 
